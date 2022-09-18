0b9543e26bb219270bfd71d5eccf5b53ca2e8580
Hovakimyan <liovaswagg@hotmail.com>
2022-09-18 22:00:07 +0200

    1.1.4

--- main.c
+++ main.c
@@ -13,8 +13,14 @@
  * @param aucun paramètre.
  * @return 0 - Arrêt normal du programme.
  */
-int main()
+int main(void)
 {
+
+//int lettre;
+//float a= lettre;
+//float b= lettre;
+//float c= lettre;
+
     printf("***********************************\n");
     printf("                 SNIR-Programme ASCII_ART\n");
     printf("***********************************\n\n");
@@ -25,7 +31,17 @@ int main()
     printf("\n");
     printf(" CCCCC\nCC    C\nCC     \nCC    C\n CCCCC \n");
     printf("\n");
+
+    char lettre = 'A',lettre2 = 'B', lettre3 = 'C';
     printf("Quelle lettre souhaitez-vous afficher (A,B ou C) ?:\n");
-    scanf("");
+    scanf("%c,&lettre");
+    printf("  AAA\n AAAAA\nAA   AA\n");
+    printf("AAAAAAA\nAA   AA\n");
+    printf("Quelle lettre souhaitez-vous afficher (A,B ou C) ?:\n");
+    scanf("%c,&B");
+    printf("BBBBB\nBB   B\nBBBBBB\nBB   BB\nBBBBBB\n");
+    printf("Quelle lettre souhaitez-vous afficher (A,B ou C) ?:\n");
+    scanf("%c,&C");
+    printf(" CCCCC\nCC    C\nCC     \nCC    C\n CCCCC \n");
     return 0;
 }
