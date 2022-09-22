44cbe184ba37d79c8b3819ed1db3e4faa82fb669
Liova Hovakimyan <liovaswagg@hotmail.com>
2022-09-22 16:58:21 +0200

    2.0

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * @file main.c
  * @brief Projet Ascii_art
  * @author Liova Hovakimyan
- * @version 1.0
+ * @version 2.0
  * @date 15/09/2022
  */
 
@@ -13,41 +13,67 @@
  * @param aucun paramètre.
  * @return 0 - Arrêt normal du programme.
  */
-int main(void)
+int main()
 {
 
     printf("***********************************\n");
     printf("                 SNIR-Programme ASCII_ARTv1.0\n");
     printf("***********************************\n\n");
-    printf("  AAA\n AAAAA\nAA   AA\n");
-    printf("AAAAAAA\nAA   AA\n");
-    printf("\n");
-    printf("BBBBB\nBB   B\nBBBBBB\nBB   BB\nBBBBBB\n");
-    printf("\n");
-    printf(" CCCCC\nCC    C\nCC     \nCC    C\n CCCCC \n");
-    printf("\n");
-
+    //printf("  AAA\n AAAAA\nAA   AA\n");
+    //printf("AAAAAAA\nAA   AA\n");
+    //printf("\n");
+    //printf("BBBBB\nBB   B\nBBBBBB\nBB   BB\nBBBBBB\n");
+    //printf("\n");
+    //printf(" CCCCC\nCC    C\nCC     \nCC    C\n CCCCC \n");
+    //printf("\n");
+    char condition;
     char lettre;
     printf("Quelle lettre souhaitez-vous afficher (A,B ou C) ?:\n");
     scanf("%c", &lettre);
+    void viderbuffer();
     printf("La lettre est %c\n", lettre);
     printf("\n");
-    if (lettre == 'A' || lettre == 'a'){
+    switch (lettre)
+    {
+    case 'A' :
         printf("  AAA\n AAAAA\nAA   AA\n");
         printf("AAAAAAA\nAA   AA\n");
-    }
-
-    if (lettre == 'B' || lettre == 'b'){
+        break;
+    case 'B' :
         printf("BBBBB\nBB   B\nBBBBBB\nBB   BB\nBBBBBB\n");
-    }
-
-    if (lettre == 'C' || lettre == 'c'){
+        break;
+    case 'C' :
         printf(" CCCCC\nCC    C\nCC     \nCC    C\n CCCCC \n");
+        break;
+    }
+    if (lettre != 'A' && lettre != 'B' && lettre != 'C') {
+    printf ("Impossible ! Veuillez choisir une lettre entre A, B et C\n");
+    printf("\n");
     }
 
-    if (lettre != 'A' && lettre != 'B' && lettre != 'C')
-        printf ("Impossible ! Veuillez choisir une lettre entre A, B et C\n");
-    printf("\n");
+
+
 
     return 0;
 }
+
+
+
+
+
+//if (lettre == 'A' || lettre == 'a'){
+// printf("  AAA\n AAAAA\nAA   AA\n");
+//printf("AAAAAAA\nAA   AA\n");
+//}
+
+//if (lettre == 'B' || lettre == 'b'){
+//printf("BBBBB\nBB   B\nBBBBBB\nBB   BB\nBBBBBB\n");
+//}
+
+//if (lettre == 'C' || lettre == 'c'){
+// printf(" CCCCC\nCC    C\nCC     \nCC    C\n CCCCC \n");
+//}
+
+//if (lettre != 'A' && lettre != 'B' && lettre != 'C')
+// printf ("Impossible ! Veuillez choisir une lettre entre A, B et C\n");
+// printf("\n");
