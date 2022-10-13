1002ae54aff7e030afe40e65d8aef0d8641f0379
Liova <liovaswagg@hotmail.com>
2022-10-13 12:31:29 +0200

    Itération_4

--- main.c
+++ main.c
@@ -9,6 +9,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
+#include "img_bmp/img_bmp.h"
+
+#pragma pack(1) // desactive l'alignement mémoire
 
 /**
  * @fn void viderBuffer(void)
@@ -42,10 +46,10 @@ char tableauA[5][8]= {{' ',' ','A','A','A',' ',' ','\0'},
                       {'A','A',' ',' ',' ','A','A','\0'}};
 
 char tableauB[5][8]= {{'B','B','B','B','B',' ',' ','\0'},
-                     {'B','B',' ',' ',' ','B',' ','\0'},
-                     {'B','B','B','B','B','B',' ','\0'},
-                     {'B','B',' ',' ',' ','B','B','\0'},
-                     {'B','B','B','B','B','B',' ','\0'}};
+                      {'B','B',' ',' ',' ','B',' ','\0'},
+                      {'B','B','B','B','B','B',' ','\0'},
+                      {'B','B',' ',' ',' ','B','B','\0'},
+                      {'B','B','B','B','B','B',' ','\0'}};
 
 char tableauC[5][8]= {{' ','C','C','C','C','C',' ','\0'},
                       {'C','C',' ',' ',' ',' ','C','\0'},
@@ -55,80 +59,99 @@ char tableauC[5][8]= {{' ','C','C','C','C','C',' ','\0'},
 
 
 void afficherAsciiArt(char caractère);
-int main()
+void sousfonctionadaptee(void)
 {
 
 
-    void sousfonctionadaptee(void);
-    {
+printf("***********************************\n");
+printf("                 SNIR-Programme ASCII_ART\n");
+printf("***********************************\n\n");
+
+char condition;
+char lettre;
+int choix;
+int i;
 
 
-    printf("***********************************\n");
-    printf("                 SNIR-Programme ASCII_ART\n");
-    printf("***********************************\n\n");
-
-    char condition;
-    char lettre;
-    int choix;
-    int i;
-
-
-    int longueur = strlen (tableauA);
-    for (i=0 ;i<longueur ; i++){
-        printf ("%c", longueur);
-    }
-
-    for (i=0 ; i<5 ; i++) {
-        printf("%s\n", tableauA[i]);
-    }
-        printf("\n");
-
-    for (i=0 ; i<5 ; i++) {
-        printf("%s\n", tableauB[i]);
-    }
-        printf("\n");
-
-    for (i=0 ;i<5 ; i++) {
-        printf("%s\n", tableauC[i]);
-    }
-        printf("\n");
-
-
-    do
-    {
-        printf("\n");
-        printf("Quelle lettre souhaitez-vous afficher (A,B ou C) ?:\n");
-        scanf("%c", &lettre);
-        getchar();
-        void viderBuffer();
-        printf("La lettre est %c\n", lettre);
-        printf("\n");
-
-        switch (lettre)
-        {
-
-        case 'A' :
-            printf("  AAA\n AAAAA\nAA   AA\nAAAAAAA\nAA   AA\n");
-            break;
-
-        case 'B' :
-            printf("BBBBB\nBB   B\nBBBBBB\nBB   BB\nBBBBBB\n");
-            break;
-
-        case 'C' :
-            printf(" CCCCC\nCC    C\nCC     \nCC    C\n CCCCC \n");
-            break;
-        case '*' :
-            printf (" Voulez-vous quitter le programme ?");
-
-        default :
-            printf(" Impossible ! Votre lettre doit etre A, B ou C en majuscule\n");
-        }
-        if (lettre == '*')
-        {
-            exit(0);
-        }
-    } while (choix !=1);
+int longueur = strlen (tableauA);
+for (i=0 ;i<longueur ; i++){
+    printf ("%c", longueur);
 }
+
+for (i=0 ; i<5 ; i++) {
+    printf("%s\n", tableauA[i]);
+}
+printf("\n");
+
+for (i=0 ; i<5 ; i++) {
+    printf("%s\n", tableauB[i]);
+}
+printf("\n");
+
+for (i=0 ;i<5 ; i++) {
+    printf("%s\n", tableauC[i]);
+}
+printf("\n");
+
+
+do
+{
+printf("\n");
+printf("Quelle lettre souhaitez-vous afficher (A,B ou C) ?:\n");
+scanf("%c", &lettre);
+getchar();
+void viderBuffer();
+printf("La lettre est %c\n", lettre);
+printf("\n");
+
+switch (lettre)
+{
+
+    case 'A' :
+        printf("  AAA\n AAAAA\nAA   AA\nAAAAAAA\nAA   AA\n");
+        break;
+
+    case 'B' :
+        printf("BBBBB\nBB   B\nBBBBBB\nBB   BB\nBBBBBB\n");
+        break;
+
+    case 'C' :
+        printf(" CCCCC\nCC    C\nCC     \nCC    C\n CCCCC \n");
+        break;
+    case '*' :
+        printf (" Voulez-vous quitter le programme ?");
+
+    default :
+        printf(" Impossible ! Votre lettre doit etre A, B ou C en majuscule\n");
+}
+if (lettre == '*')
+{
+    exit(0);
+}
+} while (choix !=1);
+}
+int main()
+{
+    FILE *file;
+
+    printf("Nom du fichier à ouvrir %s \n", "tux64.bmp");
+    file = fopen("ImagesBmp/tux64.bmp", "rb");
+    if (!file)  {
+        printf("Erreur ouverture \n");
+        exit(-1);
+    }
+
+    fclose(file);
+
+    char signature[2];
+    int test = fread(signature, 2, 1, file);
+    if(test != 1) {
+        printf("fread impossible \n");
+        exit(-1);
+    }
+    printf("Lecture de %c et %c \n", signature[0], signature[1]);
+
+
+
     return 0;
 }
