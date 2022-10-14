2df3f4a89948ea0c7e4c7dc6d680a658bb2ac61a
hovakimyan <liovaswagg@hotmail.com>
2022-10-14 11:40:31 +0200

    Itération_4

--- main.c
+++ main.c
@@ -45,6 +45,143 @@ char tableauC[5][8]= {{' ','C','C','C','C','C',' ','\0'},
                       {'C','C',' ',' ',' ',' ','C','\0'},
                       {' ','C','C','C','C','C',' ','\0'}};
 
+char tableauD[5][8] = {"DDDDD  \0",
+                       "DD   D \0",
+                       "DD   D \0",
+                       "DD   D \0",
+                       "DDDDD  \0"};
+
+char tableauE[5][8] = {"EEEEEEE\0",
+                       "EE     \0",
+                       "EEEEE  \0",
+                       "EE     \0",
+                       "EEEEEEE\0"};
+
+char tableauF[5][8] = {"FFFFFFF\0",
+                       "FF     \0",
+                       "FFFFF  \0",
+                       "FF     \0",
+                       "FF     \0"};
+
+char tableauG[5][8] = {" GGGGG \0",
+                       "GG     \0",
+                       "GG  GGG\0",
+                       "GG    G\0",
+                       " GGGGG \0"};
+
+char tableauH[5][8] = {"HH   HH\0",
+                       "HH   HH\0",
+                       "HHHHHHH\0",
+                       "HH   HH\0",
+                       "HH   HH\0"};
+
+char tableauI[5][8] = {"IIIIII \0",
+                       "  II   \0",
+                       "  II   \0",
+                       "  II   \0",
+                       "IIIIII \0"};
+
+char tableauJ[5][8] = {"JJJJJJ \0",
+                       "   J   \0",
+                       "   J   \0",
+                       "J  J   \0",
+                       " JJ    \0"};
+
+char tableauK[5][8] = {"KK  KK \0",
+                       "KK KK  \0",
+                       "KKKKK  \0",
+                       "KK  KK \0",
+                       "KK   KK\0"};
+
+char tableauL[5][8] = {"LL     \0",
+                       "LL     \0",
+                       "LL     \0",
+                       "LL    L\0",
+                       "LLLLLLL\0"};
+
+char tableauM[5][8] = {"MM   MM\0",
+                       "M M M M\0",
+                       "M  M  M\0",
+                       "M     M\0",
+                       "M     M\0"};
+
+char tableauN[5][8] = {"NN    N\0",
+                       "N N   N\0",
+                       "N  N  N\0",
+                       "N   N N\0",
+                       "N    NN\0"};
+
+char tableauO[5][8] = {" OOOOO \0",
+                       "OO   OO\0",
+                       "O     O\0",
+                       "OO   OO\0",
+                       " OOOOO \0"};
+
+char tableauP[5][8] = {"PPPPP  \0",
+                       "PP   P \0",
+                       "PPPPP  \0",
+                       "PP     \0",
+                       "PP     \0"};
+
+char tableauQ[5][8] = {" QQQQQ \0",
+                       "Q    Q \0",
+                       "Q  Q Q \0",
+                       " QQQQ  \0",
+                       "    Q  \0"};
+
+char tableauR[5][8] = {"RRRRR  \0",
+                       "RR   R \0",
+                       "RRRRR  \0",
+                       "RR  R  \0",
+                       "RR   R \0"};
+
+char tableauS[5][8] = {" SSSSS \0",
+                       "S      \0",
+                       " SSSSS \0",
+                       "      S\0",
+                       " SSSSS \0"};
+
+char tableauT[5][8] = {"TTTTTTT\0",
+                       "   T   \0",
+                       "   T   \0",
+                       "   T   \0",
+                       "   T   \0"};
+
+char tableauU[5][8] = {"U     U\0",
+                       "U     U\0",
+                       "U     U\0",
+                       "UU   UU\0",
+                       " UUUUU \0"};
+
+char tableauV[5][8] = {"       \0",
+                       "V     V\0",
+                       " V   V \0",
+                       "  V V  \0",
+                       "   V   \0"};
+
+char tableauW[5][8] = {"W     W\0",
+                       "W  W  W\0",
+                       "W  W  W\0",
+                       "W W W W\0",
+                       " W   W \0"};
+
+char tableauX[5][8] = {" X   X \0",
+                       "  X X  \0",
+                       "   X   \0",
+                       "  X X  \0",
+                       " X   X \0"};
+
+char tableauY[5][8] = {"Y     Y\0",
+                       " Y   Y \0",
+                       "  Y Y  \0",
+                       "   Y   \0",
+                       "   Y   \0"};
+
+char tableauZ[5][8] = {" ZZZZZ \0",
+                       "    Z  \0",
+                       "   Z   \0",
+                       "  Z    \0",
+                       " ZZZZZ \0"};
 
 /**
 * @fn void afficherAsciiArt(void)
@@ -97,6 +234,282 @@ void afficherAsciiArt(char lettre)
         }
         break;
     }
+        afficherAsciiArt('D');
+    case 'D' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauD[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('E');
+    case 'E' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauE[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('F');
+    case 'F' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauF[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('G');
+    case 'G' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauG[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('H');
+    case 'H' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauH[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('I');
+    case 'I' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauI[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('J');
+    case 'J' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauJ[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('K');
+    case 'K' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauK[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('L');
+    case 'L' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauL[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('M');
+    case 'M' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauM[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('N');
+    case 'N' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauN[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('O');
+    case 'O' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauO[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('P');
+    case 'P' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauP[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('Q');
+    case 'Q' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauQ[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('R');
+    case 'R' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauR[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('S');
+    case 'S' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauS[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('T');
+    case 'T' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauT[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('U');
+    case 'U' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauU[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('V');
+    case 'V' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauV[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('W');
+    case 'W' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauW[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('X');
+    case 'X' :
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
+
+                printf("%c", tableauX[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
+        afficherAsciiArt('Y');
+        case 'Y' :
+        {
+            for (int i=0 ; i<5 ; i++) {
+                for (int j=0 ; j<8 ; j++) {
+
+                    printf("%c", tableauY[i][j]);
+                }
+                printf("\n");
+            }
+            break;
+        }
+        afficherAsciiArt('Z');
+        case 'Z' :
+        {
+            for (int i=0 ; i<5 ; i++) {
+                for (int j=0 ; j<8 ; j++) {
+
+                    printf("%c", tableauZ[i][j]);
+                }
+                printf("\n");
+            }
+            break;
+        }
     default :
         printf(" Impossible ! Votre lettre doit etre A, B ou C en majuscule\n");
     }
@@ -165,4 +578,3 @@ int main()
 
     return 0;
 }
-
