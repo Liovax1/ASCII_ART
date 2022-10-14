a805e4c8585547e514f8549c71bb3b2afbe0a9f1
hovakimyan <liovaswagg@hotmail.com>
2022-10-14 09:29:50 +0200

    Itération-4

--- main.c
+++ main.c
@@ -17,27 +17,15 @@
 /**
  * @fn void viderBuffer(void)
  * @brief Vide le buffer de lecture clavier.
- * @fn int main (void)
- * @brief Fonction principale du programme.
- * @param aucun paramètre.
- * @return 0 - Arrêt normal du programme.
  */
 
-/**
- * @fn void afficherAsciiArt(void)
- * @brief Affiche AsciiArt
- * @fn int main (void)
- * @brief Fonction principale du programme.
- * @param char caractère
- * @return 0 - Arrêt normal du programme.
- */
-
-/**
-* @fn int main (void)
-* @brief Fonction principale du programme.
-* @param aucun paramètre.
-* @return 0 - Arrêt normal du programme.
-*/
+void viderBuffer()
+{
+    int c = 0;
+    while (c != '\n' && c != EOF) {
+        c = getchar();
+    }
+}
 
 char tableauA[5][8]= {{' ',' ','A','A','A',' ',' ','\0'},
                       {' ','A','A','A','A','A',' ','\0'},
@@ -58,100 +46,113 @@ char tableauC[5][8]= {{' ','C','C','C','C','C',' ','\0'},
                       {' ','C','C','C','C','C',' ','\0'}};
 
 
-void afficherAsciiArt(char caractère);
-void sousfonctionadaptee(void)
+
+void afficherAsciiArt(char lettre);
+void afficherPhrase(void);
+void sousfonctionadaptee(void);
+
+
+void afficherAsciiArt(char lettre)
 {
+    lettre = toupper(lettre);
 
-
-printf("***********************************\n");
-printf("                 SNIR-Programme ASCII_ART\n");
-printf("***********************************\n\n");
-
-char condition;
-char lettre;
-int choix;
-int i;
-
-
-int longueur = strlen (tableauA);
-for (i=0 ;i<longueur ; i++){
-    printf ("%c", longueur);
-}
-
-for (i=0 ; i<5 ; i++) {
-    printf("%s\n", tableauA[i]);
-}
-printf("\n");
-
-for (i=0 ; i<5 ; i++) {
-    printf("%s\n", tableauB[i]);
-}
-printf("\n");
-
-for (i=0 ;i<5 ; i++) {
-    printf("%s\n", tableauC[i]);
-}
-printf("\n");
-
-
-do
-{
-printf("\n");
-printf("Quelle lettre souhaitez-vous afficher (A,B ou C) ?:\n");
-scanf("%c", &lettre);
-getchar();
-void viderBuffer();
-printf("La lettre est %c\n", lettre);
-printf("\n");
-
-switch (lettre)
-{
-
+    switch(lettre){
+    afficherAsciiArt('A');
     case 'A' :
-        printf("  AAA\n AAAAA\nAA   AA\nAAAAAAA\nAA   AA\n");
-        break;
+    {
+        for (int i=0 ; i<5 ; i++) {
+            for (int j=0 ; j<8 ; j++) {
 
-    case 'B' :
-        printf("BBBBB\nBB   B\nBBBBBB\nBB   BB\nBBBBBB\n");
+                printf("%c", tableauA[i][j]);
+            }
+            printf("\n");
+        }
         break;
-
-    case 'C' :
-        printf(" CCCCC\nCC    C\nCC     \nCC    C\n CCCCC \n");
+    }
+        afficherAsciiArt('B');
+    case 'B' : {
+        for (int i = 0 ; i < 5 ; i ++)  {
+            for (int j = 0 ; j < 8 ; j++)   {
+                printf("%c", tableauB[i][j]);
+            }
+            printf("\n");
+        }
         break;
-    case '*' :
-        printf (" Voulez-vous quitter le programme ?");
-
+    }
+        afficherAsciiArt('C');
+    case 'C' : {
+        for (int i = 0 ; i < 5 ; i ++)  {
+            for (int j = 0 ; j < 8 ; j++)   {
+                printf("%c", tableauC[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+    }
     default :
         printf(" Impossible ! Votre lettre doit etre A, B ou C en majuscule\n");
+    }
 }
-if (lettre == '*')
+
+void afficherPhrase(void)
 {
-    exit(0);
+    char sortie;
+    sortie = '*';
+    do
+    {
+
+        printf("***********************************\n");
+        printf("                 SNIR-Programme ASCII_ART\n");
+        printf("***********************************\n\n");
+        char lettre;
+        char phrase[100];
+        printf("Quelle lettre souhaitez-vous afficher (A,B ou C) ?:\n");
+        scanf("%c", &lettre);
+        afficherAsciiArt(lettre);
+        viderBuffer();
+        printf("La lettre est %c\n", lettre);
+        printf("\n");
+        int longueur = strlen(phrase);
+        int i;
+        for (i=0 ; i<longueur ; i++){
+            afficherAsciiArt(phrase[i]);
+        }
+        printf("Voulez-vous arrêter le programme ? Appuyez sur *.\n\n");
+        scanf("%c", &sortie);
+        viderBuffer();
+    }
+    while (sortie != '*');
+    printf("\Sortie du programme\n\n");
 }
-} while (choix !=1);
-}
+
+
+/**
+* @fn int main (void)
+* @brief Fonction principale du programme.
+* @param aucun paramètre.
+* @return 0 - Arrêt normal du programme.
+*/
+
 int main()
 {
     FILE *file;
 
-    printf("Nom du fichier à ouvrir %s \n", "tux64.bmp");
-    file = fopen("ImagesBmp/tux64.bmp", "rb");
+    printf("Nom du fichier a ouvrir: %s \n", "tux64.bmp");
+    file = fopen("/ImagesBmp/tux64.bmp", "rb");
     if (!file)  {
         printf("Erreur ouverture \n");
         exit(-1);
     }
-
-    fclose(file);
-
     char signature[2];
+    int taille [1];
     int test = fread(signature, 2, 1, file);
     if(test != 1) {
         printf("fread impossible \n");
         exit(-1);
     }
     printf("Lecture de %c et %c \n", signature[0], signature[1]);
-
-
-
+    printf("Taille totale du fichier: %d %d %d %d\n", taille[0]);
+    fclose(file);
     return 0;
 }
+
